183ddf4479413c49a7fe9eee7d2b09a9e1de0c56
hitomi numari <h.h.hitomi1216@gmail.com>
2020-04-05 00:44:55 +0900

    Uploader作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, on: :create
   has_many :pictures
+  mount_uploader :image, UserImageUploader
+
   def User.digest(string)
    cost = ActiveModel::SecurePassword.min_cost ? Bcrypt::Engine::MIN_COST :
                                                 BCrypt::Engine.cost
