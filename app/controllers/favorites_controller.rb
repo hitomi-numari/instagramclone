7478c7539f091b307fb716bf897384ba880f815a
hitomi numari <h.h.hitomi1216@gmail.com>
2020-04-08 20:13:43 +0900

    undefined method ‘destroy’ for nil:NilClass.解消

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,14 @@
 class FavoritesController < ApplicationController
   def index
   end
-binding.pry
+
   def create
     favorite = current_user.favorites.create(picture_id: params[:picture_id])
     redirect_to pictures_url, notice: "#{favorite.picture.user.name}さんの写真をお気に入り登録しました"
   end
 
   def destroy
+    # binding.pry
     favorite = current_user.favorites.find_by(id: params[:id]).destroy
     redirect_to pictures_url, notice: "#{favorite.picture.user.name}さんの写真をお気に入り解除しました"
   end
