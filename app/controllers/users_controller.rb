05708a94274bd302944a3d00a91c485cc757e516
hitomi numari <h.h.hitomi1216@gmail.com>
2020-04-11 03:05:27 +0900

    ロケット法修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to new_session_path
     else
       render :new
@@ -37,7 +38,7 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, 
+    params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation, :image)
   end
 end
